c44b6860eab9e6171777ff4e7773cecf47d8eaa0
SohamChk <career.soham.dev@gmail.com>
2024-03-17 23:16:56 +0530

    dynamic memepry allocation for config

--- src/config.c
+++ src/config.c
@@ -7,6 +7,8 @@
 
 void loadConfig() {
     char buffer[1024];  // buffer to read config file
+    char* configData;
+    long fileLength;
 
     // open the config JSON
     FILE *fp = fopen(PROXY_CONFIG, "r");    // `PROXY_CONFIG` is set in the `constants.h` file
@@ -15,10 +17,20 @@ void loadConfig() {
         return 1; // Return a non-zero value 
     }
 
+    // Get file length
+    fseek(fp, 0, SEEK_END);
+    fileLength = ftell(fp);
+    rewind(fp);
+
+    // Dynamically allocate memory for config file content
+    configData = (char *)malloc(fileLength * sizeof(char));
+
     // Read file line by line and print its contents
     while (fgets(buffer, sizeof(buffer), fp) != NULL) {
-        
+        strcat(configData, buffer);
     }
+
+    printf("config %s\n", configData);
 
     // Close the file
     fclose(fp);
